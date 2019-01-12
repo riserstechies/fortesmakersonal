f76750ceb0744fc4ec8c58c5743c5a8bfc04a2a8
riserstechies <46120655+riserstechies@users.noreply.github.com>
2019-01-12 23:08:59 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 <html lang="en-US">
     <head>
         <meta charset="UTF-8">
-        <meta http-equiv="refresh" content="1; url=https://www.youtube.com/user/IanH/videos">   
+        <meta http-equiv="refresh" content="1; url=https://www.amazon.com/BlenderBottle-ProStak-22-Ounce-Bottle-Storage/dp/B01LXRWZNI/ref=sr_1_42?srs=18505439011&ie=UTF8&qid=1547314535&sr=8-42">   
         <script type="text/javascript">
-               window.location.href = "https://www.youtube.com/user/IanH/videos"
+               window.location.href = "https://www.amazon.com/BlenderBottle-ProStak-22-Ounce-Bottle-Storage/dp/B01LXRWZNI/ref=sr_1_42?srs=18505439011&ie=UTF8&qid=1547314535&sr=8-42"
         </script>
         <title>Page Redirection</title>
     </head>
